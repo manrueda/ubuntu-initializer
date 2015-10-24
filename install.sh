5fbf17502ec48b78737b32c4daa5054ea5665f8c
Manuel Rueda <manuel.rueda.un@gmail.com>
2015-10-24 15:10:52 -0300

    basic structure

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+clear
+
+echo "This script will create some custom folders, install a lot of basic applications, etc..."
+
+echo "Creating folders"
+
+echo "Installing apt applications"
+
+echo "Installing other applications"
